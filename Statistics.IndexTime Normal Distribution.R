99b51d01e007735e5d073b402b55f0f778584065
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2021-09-04 21:55:32 +0200

    Add files via upload

---
+++ Statistics.IndexTime Normal Distribution.R
@@ -0,0 +1,24 @@
+#Multiple Normal Distribution
+library(readxl)
+library(ggplot2)
+summary(IndexTiempo)
+
+IndexTiempo <- read.csv2("/Users/wemigliari/Documents/R/tabelas/IndexTiempoSPTER.csv")
+
+MinutosCTrab <- IndexTiempo$MinutosCTrabSP
+MinutosCEB <- IndexTiempo$MinutosCTrabTER
+
+par(mfrow = c(1,1), family= "Arial", cex = 0.5, oma = c(4, 1, 1, 4))
+
+ggplot(IndexTiempo, aes(MinutosCTrab, fil = as.factor(MinutosCEB))) + geom_density(alpha = 0.3, col = 'lightblue', show.legend = TRUE) +
+  labs(x = "Tempo", y = "Distribuição de Probabilidade") + geom_vline(xintercept = 60, size = 0.5, col = "red")
+
+
+#Remembering the normal distribution
+
+mean(MinutosCTrab)
+sd(MinutosCTrab)
+
+par(mfrow = c(1,1), family= "Arial", cex = 0.7, oma = c(4, 1, 1, 4))
+plot(function(MinutosCTrab)dnorm(MinutosCTrab,mean=44.7,sd=25.49),1,95,
+     xlab="Tempo", ylab= "Distribuição de Probabilidade")
